a95568c47fc3c29dfff4dc020b21a9e0d90393d4
abefimrs <mail2asifsana@gmail.com>
2015-09-17 13:17:39 +0600

    thumbnail improvation

--- exceptionalHandeling/fetal_error.php
+++ exceptionalHandeling/fetal_error.php
@@ -5,23 +5,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+    register_shutdown_function('fatalErrorHandler');
     require_once("model/model.php");
 
     function fatalErrorHandler() {
         
         # Getting last error
             $error = error_get_last();
+            //print_r($error);
         # Checking if last error is a fatal error
             if (($error['type'] === E_ERROR) || ($error['type'] === E_USER_ERROR)) {
         # Here we handle the error, displaying HTML, logging, ...
                 echo 'Sorry, a serious error has occured in ' . $error['file'];
             }
             
-            echo "sdfdsf";
+            
     }
 
     # Registering shutdown function
-    register_shutdown_function('fatalErrorHandler');
+
 
    
